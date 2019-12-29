f03e2117a9073cf51b52b7e8cdc70a385f6001c5
Alex <ipoopfool@gmail.com>
2019-12-29 17:05:18 -0500

    rewrite board and piece classes to more single responsibility

--- board.js
+++ board.js
@@ -1,16 +1,13 @@
 class Board {
-    grid;
-    piece;
+    grid; // arranged by [y][x]
 
-    // Reset the board when we start a new game
-    reset() {
-        this.grid = this.getEmptyBoard();
+    constructor() {
+        this.reset();
     }
 
-    // Generate a matrix filled with zeros
-    // arranged by [y][x]
-    getEmptyBoard() {
-        return Array.from({length: ROWS}, () => Array(COLS).fill(0));
+    // set/reset the grid to a matrix filled with zeros
+    reset() {
+        this.grid = Array.from({length: ROWS}, () => Array(COLS).fill(0));
     }
 
     valid(piece) {
@@ -21,59 +18,43 @@ class Board {
 
                 return (
                     value === 0 || 
-                    (this.isInsideWalls(x) && this.isAboveFloor(y) && this.isNotOccupied(x, y))
+                    (_isInsideWalls(x) && _isAboveFloor(y) && _isNotOccupied(this.grid[y][x]))
                 );
             });
         });
     }
 
-    isNotOccupied(x, y) {
-        return this.grid[y][x] === 0;
-    }
+    // drop() {
+    //     let p = moves[KEY.DOWN](this.piece);
 
-    isInsideWalls(x) {
-        return x >= 0 && x < COLS;
-    }
+    //     if (this.valid(p)) {
+    //         this.piece.move(p);
+    //     }
+    //     else {
+    //         // this.freeze();
+    //         // this.clearLines();
+    //         // ...
+    //     }
+    // }
 
-    isAboveFloor(y) {
-        return y < ROWS;
-    }
+    // draw() {
+    //     this.grid.forEach((row, y) => {
+    //         row.forEach((value, x) => {
+    //             this.ctx.fillStyle = COLORS[value];
+    //             this.ctx.fillRect(x, y, 1, 1);
+    //         });
+    //     });
+    // }
+}
 
-    rotate(piece) {
-        // Deep clone the piece to copy it's 2d shape array correctly
-        let newPiece = JSON.parse(JSON.stringify(piece));
+function _isNotOccupied(cellValue) {
+    return cellValue === 0;
+}
 
-        // Transpose matrix
-        for (let y = 0; y < newPiece.shape.length; ++y) {
-            for (let x = 0; x < y; ++x) {
-                [newPiece.shape[x][y], newPiece.shape[y][x]] = [newPiece.shape[y][x], newPiece.shape[x][y]];
-            }
-        }
+function _isInsideWalls(x) {
+    return x >= 0 && x < COLS;
+}
 
-        // Reverse the order of the columns
-        newPiece.shape.forEach(row => row.reverse());
-        return newPiece;
-    }
-
-    drop() {
-        let p = moves[KEY.DOWN](this.piece);
-
-        if (this.valid(p)) {
-            this.piece.move(p);
-        }
-        else {
-            // this.freeze();
-            // this.clearLines();
-            // ...
-        }
-    }
-
-    draw() {
-        this.grid.forEach((row, y) => {
-            row.forEach((value, x) => {
-                this.ctx.fillStyle = COLORS[value];
-                this.ctx.fillRect(x, y, 1, 1);
-            });
-        });
-    }
+function _isAboveFloor(y) {
+    return y < ROWS;
 }
--- piece.js
+++ piece.js
@@ -1,42 +1,67 @@
-class Piece { 
-    x;
-    y;
+class Piece {
+    x; // mutate directly to move
+    y; // mutate directly to move
     color;
     shape;
-    ctx;
 
-    constructor(ctx) {
-        this.ctx = ctx;
-        this.spawn();
+    constructor(piece) {
+        // Optional constructor param to create a copy of an existing piece
+        // Probably use clone() to be explicit when creating a copied class instance
+        if (piece !== null) {
+            this.x = piece.x;
+            this.y = piece.y;
+            this.color = piece.color;
+            this.shape = piece.shape;
+        }
+        else {
+            const typeId = _randomizeTetronimoType(COLORS.length);
+
+            this.color = COLORS[typeId];
+            this.shape = SHAPES[typeId];
+            this.x = 3;
+            this.y = 0;
+        }
     }
 
-    spawn() {
-        const typeId = this.randomizeTetronimoType(COLORS.length);
-        this.color = COLORS[typeId];
-        this.shape = SHAPES[typeId];
-
-        this.x = 3;
-        this.y = 0;
+    clone() {
+        return new Piece(this);
     }
 
-    draw() {
-        this.ctx.fillStyle = this.color;
-        this.shape.forEach((row, y) => {
-            row.forEach((value, x) => {
-                if (value > 0) {
-                    this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
-                }
-            });
-        });
-    }
+    // draw() {
+    //     this.ctx.fillStyle = this.color;
+    //     this.shape.forEach((row, y) => {
+    //         row.forEach((value, x) => {
+    //             if (value > 0) {
+    //                 this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
+    //             }
+    //         });
+    //     });
+    // }
 
-    move(piece) {
-        this.x = piece.x;
-        this.y = piece.y;
-        this.shape = piece.shape;
-    }
+    // move(piece) {
+    //     this.x = piece.x;
+    //     this.y = piece.y;
+    //     this.shape = piece.shape;
+    // }
 
-    randomizeTetronimoType(numTypes) {
-        return Math.floor(Math.random() * numTypes);
+    rotate() {
+        // Deep clone the shape so we can mutate the 2d array
+        let newShape = JSON.parse(JSON.stringify(piece));
+
+        // Transpose matrix
+        for (let y = 0; y < newShape.length; ++y) {
+            for (let x = 0; x < y; ++x) {
+                [newShape[x][y], newShape[y][x]] = [newShape[y][x], newShape[x][y]];
+            }
+        }
+
+        // Reverse the order of the columns
+        newShape.forEach(row => row.reverse());
+
+        this.shape = newShape;
     }
+}
+
+function _randomizeTetronimoType(numTypes) {
+    return Math.floor(Math.random() * numTypes);
 }
